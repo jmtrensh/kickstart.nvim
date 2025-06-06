4386a5624b7b3bc685ff90ba09201f103f771d2b
Joe Trenshaw <joseph.trenshaw@geaerospace.com>
2025-06-06 11:54:13 -0400

    Remove window size for netrw

--- init.lua
+++ init.lua
@@ -164,7 +164,7 @@ vim.opt.confirm = true
 -- Netrw settings:
 vim.g.netrw_keepdir = 0
 vim.g.netrw_banner = 0
-vim.g.netrw_winsize = 30
+--vim.g.netrw_winsize = 30
 vim.g.netrw_browsex_viewer = '-'
 
 -- [[ Basic Keymaps ]]
